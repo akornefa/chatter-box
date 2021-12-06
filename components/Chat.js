e79dd22624a2cc7e9d0e9ebff263021e6a397c43
Akornefa Akyea <64564297+akornefa@users.noreply.github.com>
2021-12-06 15:52:32 -0500

    Import and configure GiftedChat and Bubble
    These GiftedChat is fed messages from the message state. Bubble simply changes the background color of chat messages

--- components/Chat.js
+++ components/Chat.js
@@ -1,27 +1,95 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet, Platform, KeyboardAvoidingView } from 'react-native';
+import { Bubble, GiftedChat } from 'react-native-gifted-chat';
 
 export default class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    }
+  }
 
-  render() {
+  componentDidMount() {
     let name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name });
-    let { bkgColor } = this.props.route.params;
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: `Hello, ${name}`,
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any'
+          },
+        },
+        {
+          _id: 2,
+          text: 'This is a system message',
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    })
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
+  //change message bubble color
+  renderBubble(props) {
     return (
-      <View style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: bkgColor,
-      }}>
-        <Text>Hello Chat!</Text>
-        <Button
-          title='Go to Start'
-          onPress={() => this.props.navigation.navigate('Start')}
-        />
-
-      </View>
-
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#000'
+          }
+        }}
+      />
     )
   }
+
+  render() {
+
+    let { bkgColor } = this.props.route.params;
+    return (
+      <View style={[styles.container, { backgroundColor: bkgColor }]}>
+        <View style={styles.giftedChat}>
+          <GiftedChat
+            renderBubble={this.renderBubble.bind(this)}
+            messages={this.state.messages}
+            onSend={(messages) => this.onSend(messages)}
+            user={{
+              _id: 1,
+            }}
+          />
+          <Button
+            title='Go to Start'
+            onPress={() => this.props.navigation.navigate('Start')}
+          />
+          {Platform.OS === 'android' ? <KeyboardAvoidingView behavior='height' /> : null}
+        </View>
+      </View>
+
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  giftedChat: {
+    flex: 1,
+    width: '88%',
+    paddingBottom: 10
+  }
+});
